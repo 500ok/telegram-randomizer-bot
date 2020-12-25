bf4c074831ef321941b147c1e66b56574b057a19
500ok <74183645+500ok@users.noreply.github.com>
2020-12-25 23:11:47 +0500

    removed useless annotations
    fix

--- src/main/java/org/randomizer/App.java
+++ src/main/java/org/randomizer/App.java
@@ -1,15 +1,11 @@
 package org.randomizer;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@Slf4j
-@ComponentScan
 @SpringBootApplication
 public class App {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 }
