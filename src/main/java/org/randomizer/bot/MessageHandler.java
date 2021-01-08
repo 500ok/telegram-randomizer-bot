82f19d797664f7063d2a2b199839f8cf322159b9
500ok <grachevm7@gmail.com>
2021-01-08 21:54:38 +0500

    update

--- src/main/java/org/randomizer/bot/MessageHandler.java
+++ src/main/java/org/randomizer/bot/MessageHandler.java
@@ -1,26 +1,44 @@
 package org.randomizer.bot;
 
 import lombok.extern.slf4j.Slf4j;
-import org.randomizer.model.FormattedMessage;
+import org.randomizer.bot.formatter.MessageFormatter;
+import org.randomizer.bot.menu.MenuState;
+import org.randomizer.bot.menu.MenuView;
+import org.randomizer.model.Game;
+import org.randomizer.model.Movie;
 import org.randomizer.randomizer.GameRandomizer;
 import org.randomizer.randomizer.MovieRandomizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
+import java.util.Map;
+
 @Slf4j
 @Component
-public class CommandExecutor {
+public class MessageHandler {
 
+    @Autowired
+    private MessageFormatter<Game> gameMessageFormatter;
+    @Autowired
+    private MessageFormatter<Movie> movieMessageFormatter;
     @Autowired
     private GameRandomizer gameRandomizer;
     @Autowired
     private MovieRandomizer movieRandomizer;
 
-    public SendMessage execute(Message message) {
-        SendMessage sendMessage;
-        FormattedMessage formattedMessage;
+    private final Map<MenuState ,MenuView> menus;
+
+    @Autowired
+    public MessageHandler(Map<MenuState, MenuView> menus) {
+        this.menus = menus;
+    }
+
+    public BotApiMethod<?> handle(Message message) {
+        BotApiMethod<?> sendMessage;
+        String messageText;
         String command = message.getText();
         String chatId = String.valueOf(message.getChatId());
 
@@ -28,10 +46,10 @@ public class CommandExecutor {
 
         switch (command){
             case "/game":
-                formattedMessage = gameRandomizer.getRandomGame();
+                messageText = gameMessageFormatter.getFormattedMessage(gameRandomizer.getRandomGame());
                 break;
             case "/movie":
-                formattedMessage = movieRandomizer.getRandomMovie();
+                messageText = movieMessageFormatter.getFormattedMessage(movieRandomizer.getRandomMovie());
                 break;
             default:
                 return SendMessage.builder()
@@ -40,15 +58,14 @@ public class CommandExecutor {
                         .build();
         }
 
-        if (formattedMessage != null) {
+        if (messageText != null) {
             sendMessage = SendMessage.builder()
                     .chatId(String.valueOf(message.getChatId()))
                     .parseMode("markdown")
-                    .text(formattedMessage.getFormattedMessage())
+                    .text(messageText)
                     .build();
 
-            log.debug("Message {} was sent with length {}",
-                    message.getChatId(), sendMessage.getText().length());
+            log.debug("Message {} was sent.", message.getChatId());
 
             return sendMessage;
         } else {
